ebc8b62e766177f03fd7a5b4020c056de8e77b37
Joseph Kisbye <josephkisbye@gmail.com>
2025-02-17 13:45:56 -0500

    zone redesign

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,16 +6,38 @@ function changeSection(id) {
 
 function lightsOut() {
   $("body").addClass("dark");
-  $("h1").addClass("dark");
-  $("p").addClass("dark");
-  $("a").addClass("dark");
+  $("h1").addClass("dark-text");
+  $("h2").addClass("dark-text");
+  $("h3").addClass("dark-text");
+  $("h4").addClass("dark-text");
+  $("p").addClass("dark-text");
+  $("a").addClass("dark-text");
+  $("footer").addClass("dark-nav");
+  $("nav").addClass("dark-nav");
+  $("#home-link").addClass("dark-nav-text");
+  $("#about-link").addClass("dark-nav-text");
+  $("#name-link").addClass("dark-nav-text")
+  $("#foot-text").addClass("dark-nav-text")
+  $("#foot-name-link").css("color", "var(--gray)")
+
+  
 }
 
 function lightsOn() {
   $("body").removeClass("dark");
-  $("h1").removeClass("dark");
-  $("p").removeClass("dark");
-  $("a").removeClass("dark");
+  $("h1").removeClass("dark-text");
+  $("h2").removeClass("dark-text");
+  $("h3").removeClass("dark-text");
+  $("h4").removeClass("dark-text");
+  $("p").removeClass("dark-text");
+  $("a").removeClass("dark-text");
+  $("footer").removeClass("dark-nav");
+  $("nav").removeClass("dark-nav");
+  $("#home-link").removeClass("dark-nav-text");
+  $("#about-link").removeClass("dark-nav-text");
+  $("#name-link").removeClass("dark-nav-text")
+  $("#foot-text").removeClass("dark-nav-text")
+  $("#foot-name-link").css("color", "var(--amber)")
 }
 
 $(function () {
